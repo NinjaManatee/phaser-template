ff8cc38cf673b0b563c103b3fed67f379d02f796
NinjaManatee <andy@chartiq.com>
2017-12-01 18:52:51 -0500

    Added configuration for webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ module.exports = {
 	entry: "./src/entry.js",
 	output: {
 		filename: "bundle.js",
-		path: path.resolve(__dirname, "dist/js")
+		path: path.resolve(__dirname, "dist/js"),
+		publicPath: "/js/"
+	},
+	devServer: {
+		contentBase: path.join(__dirname, "dist")
 	}
 };
